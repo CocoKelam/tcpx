71e0d317abdcc20961561200194ce639e79ca05d
kakashi <ap0906106@163.com>
2024-04-22 10:52:52 +0800

    新增注册服务端向客户端心跳响应函数示例代码

--- examples/modules/heartbeat/server/server.go
+++ examples/modules/heartbeat/server/server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/CocoKelam/tcpx"
 	//"tcpx"
 	"time"
@@ -14,6 +17,11 @@ func main() {
 
 	// donot send client heartbeat
 	srv.ClientHBModeDetail(true, 5*time.Second, true, tcpx.DEFAULT_HEARTBEAT_MESSAGEID)
+	srv.HBToClientMode(true, 5*time.Second, func(c *tcpx.Context) {
+		if err := c.JSON(10088, nil, nil); err != nil {
+			log.Println(fmt.Sprintf("send heartbeat to client '%s'", c.ClientIP()))
+		}
+	})
 
 	//srv.RewriteHeartBeatHandler(1300, func(c *tcpx.Context) {
 	//	fmt.Println("rewrite heartbeat handler")
